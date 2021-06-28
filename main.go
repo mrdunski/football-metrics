d086609965ebda9a6f7576fe6d7c57276541f786
Patryk Dunski <patryk.dunski@kende.pl>
2021-06-28 11:30:02 +0200

    Single pool of teams

--- main.go
+++ main.go
@@ -9,110 +9,38 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 )
 
-var poland = football.Team{
-	Name: "Polska",
-	Players: []football.Player{
-		{
-			Name:    "Robert Lewandowski",
-			Offence: 0.05,
-			BadLuck: 0,
-		},
-		{
-			Name:    "Wojciech Szczęsny",
-			Offence: 0,
-			BadLuck: 0.03,
-		},
-		{
-			Name:    "Jakub Błaszczykowski",
-			Offence: 0.01,
-			BadLuck: 0,
-		},
-		{
-			Name:    "Arkadiusz Milik",
-			Offence: 0.01,
-			BadLuck: 0,
-		},
-		{
-			Name:    "Grzegorz Krychowiak",
-			Offence: 0.01,
-			BadLuck: 0,
-		},
-		{
-			Name:    "Kamil Glik",
-			Offence: 0,
-			BadLuck: 0,
-		},
-		{
-			Name:    "Piotr Zieliński",
-			Offence: 0.01,
-			BadLuck: 0,
-		},
-		{
-			Name:    "Kamil Jóźwiak",
-			Offence: 0.01,
-			BadLuck: 0,
-		},
-		{
-			Name:    "Karol Świderski",
-			Offence: 0.01,
-			BadLuck: 0,
-		},
-		{
-			Name:    "Jakub Moder",
-			Offence: 0.01,
-			BadLuck: 0,
-		},
-		{
-			Name:    "Michał Helik",
-			Offence: 0,
-			BadLuck: 0,
-		},
-	},
-	Defence: 0.5,
-}
-
-var otherTeams = []string{
-	"Niemcy",
-	"Włochy",
-	"Hiszpania",
-	"Wielka Brytania",
-	"Norwegia",
-	"Szwecja",
-	"Portugalia",
-	"Białoruś",
-	"Czechy",
-	"Litwa",
-	"Dania",
-	"Białoruś",
-	"Chorwacja",
-}
-
-func randomMatcher(teams []football.Team) {
-	hosts := teams[rand.Intn(len(teams))]
-	guests := teams[rand.Intn(len(teams))]
-
-	for hosts.Name == guests.Name {
-		guests = teams[rand.Intn(len(teams))]
-	}
-
+func play(hosts, guests football.Team, group *sync.WaitGroup) {
+	time.Sleep(time.Duration(rand.Intn(120)) * time.Second)
 	football.CreateMatch(hosts, guests).Play()
+	group.Done()
 }
 
-func runTournament() {
-	teams := make([]football.Team, len(otherTeams)+1)
-	teams[0] = poland
-
-	for i, team := range otherTeams {
-		teams[i+1] = randomizer.RandomTeam(team)
-	}
+func runTournament(concurrency int) {
+	teams := randomizer.RandomTeams()
 
 	for true {
-		time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
-		randomMatcher(teams)
-		time.Sleep(time.Duration(rand.Intn(90)) * time.Minute)
+		group := sync.WaitGroup{}
+		rand.Shuffle(len(teams), func(a, b int) {
+			teamA := teams[a]
+			teamB := teams[b]
+
+			teams[a] = teamB
+			teams[b] = teamA
+
+		})
+		for i := 0; i < concurrency; i++ {
+			hosts := teams[i*2]
+			guests := teams[i*2+1]
+
+			group.Add(1)
+			go play(hosts, guests, &group)
+		}
+
+		group.Wait()
 	}
 }
 
@@ -120,10 +48,7 @@ func main() {
 	address := getListeningAddress()
 	fmt.Printf("Listening on %s\n", address)
 
-	go runTournament()
-	go runTournament()
-	go runTournament()
-	go runTournament()
+	go runTournament(6)
 
 	http.HandleFunc("/", home)
 	http.HandleFunc("/health", health)
---
+++ randomizer/random_teams.go
@@ -0,0 +1,92 @@
+package randomizer
+
+import "football-metrics/football"
+
+var poland = football.Team{
+	Name: "Polska",
+	Players: []football.Player{
+		{
+			Name:    "Robert Lewandowski",
+			Offence: 0.05,
+			BadLuck: 0,
+		},
+		{
+			Name:    "Wojciech Szczęsny",
+			Offence: 0,
+			BadLuck: 0.03,
+		},
+		{
+			Name:    "Jakub Błaszczykowski",
+			Offence: 0.01,
+			BadLuck: 0,
+		},
+		{
+			Name:    "Arkadiusz Milik",
+			Offence: 0.01,
+			BadLuck: 0,
+		},
+		{
+			Name:    "Grzegorz Krychowiak",
+			Offence: 0.01,
+			BadLuck: 0,
+		},
+		{
+			Name:    "Kamil Glik",
+			Offence: 0,
+			BadLuck: 0,
+		},
+		{
+			Name:    "Piotr Zieliński",
+			Offence: 0.01,
+			BadLuck: 0,
+		},
+		{
+			Name:    "Kamil Jóźwiak",
+			Offence: 0.01,
+			BadLuck: 0,
+		},
+		{
+			Name:    "Karol Świderski",
+			Offence: 0.01,
+			BadLuck: 0,
+		},
+		{
+			Name:    "Jakub Moder",
+			Offence: 0.01,
+			BadLuck: 0,
+		},
+		{
+			Name:    "Michał Helik",
+			Offence: 0,
+			BadLuck: 0,
+		},
+	},
+	Defence: 0.5,
+}
+
+var otherTeams = []string{
+	"Niemcy",
+	"Włochy",
+	"Hiszpania",
+	"Wielka Brytania",
+	"Norwegia",
+	"Szwecja",
+	"Portugalia",
+	"Białoruś",
+	"Czechy",
+	"Litwa",
+	"Dania",
+	"Białoruś",
+	"Chorwacja",
+}
+
+func RandomTeams() []football.Team {
+	teams := make([]football.Team, len(otherTeams)+1)
+	teams[0] = poland
+
+	for i, team := range otherTeams {
+		teams[i+1] = RandomTeam(team)
+	}
+
+	return teams
+}
